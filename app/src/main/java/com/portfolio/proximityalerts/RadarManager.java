0772d0f3d77ab9050e3ad39a54a868b2ed8eaf2c
OdedBlech <unconfigured@null.spigotmc.org>
2022-10-26 15:27:27 +0300

    log messages

--- app/src/main/java/com/portfolio/proximityalerts/RadarManager.java
+++ app/src/main/java/com/portfolio/proximityalerts/RadarManager.java
@@ -1,5 +1,7 @@
 package com.portfolio.proximityalerts;
 
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -28,7 +30,9 @@ public class RadarManager {
     public HashMap<String, EncounterView> setEncounter(EncounterView view){
         if(encounters.containsKey(view.mmsi)){
             encounters.get(view.mmsi).updateView(view);
+            Log.e(TAG, "updating new view");
         }else{
+            Log.e(TAG, "adding new view");
             encounters.put(view.mmsi, view);
         }
         return encounters;
